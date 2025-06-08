efe7db0c9b5e4ad176da665008bd3b55bc5f0bbc
Mathéo Eymar <matheocarle59@gmail.com>
2025-06-08 19:49:23 +0200

    modèle nextLetter prêt

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 import * as R from "ramda";
 import fs from "node:fs";
 
+const normalize = word=>word.normalize("NFC").toLowerCase()
+
 const nextLetterModel = (data,word) => {
     return R.pipe(
         R.split('\n'),
         R.filter(R.startsWith(word)),
-        R.map(word=>word.normalize("NFC").toLowerCase()),
+        R.map(normalize),
         R.map(R.drop(word.length)),
+        R.map(R.head),
+        R.countBy(R.identity),
+        R.toPairs,
+        R.sort(([, a], [, b]) => b - a),
+        //R.take(3)
         )(data);
 }
 
